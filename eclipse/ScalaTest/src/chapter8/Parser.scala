55071f63a0b7dd74d6cf2e6f12a87f3f986f4fb0
Sasaki-Riki <unagipretz7@gmail.com>
2017-08-28 03:43:47 +0900

    scalaエラー解消のためコミット

--- eclipse/ScalaTest/src/chapter8/Parser.scala
+++ eclipse/ScalaTest/src/chapter8/Parser.scala
@@ -20,21 +20,21 @@ object parser extends App {
   
   // コンビネータ
   // 他の関数を引数に、関数を返す関数のこと 関数だらけ。。
-  def parse = (elem('t')~elem('r')~elem('u')~elem('e')) | (elem('f')~elem('a')~elem('l')~elem('s')~elem('e'))
-  def p2 = ('t'~'r'~'u'~'e') | ('f'~'a'~'l'~'s'~'e')
-  // ^^^：　マッチした場合の戻り値を指定できる。
-  def p3: Parser[Boolean] = ('t'~'r'~'u'~'e' ^^^ true) | ('f'~'a'~'l'~'s'~'e' ^^^ false)
-  def p4: Parser[Boolean] = ('t'~'r'~'u'~'e') | ('f'~'a'~'l'~'s'~'e')
-  
-  def positiveDigit = elem('1') | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 
-  def digit = positiveDigit | '0'
-  // ^^：　マッチした場合にパターンに含まれる文字の一部を返却できる
-  def long1: Parser[Long] = positiveDigit ~ rep(digit) ^^ {
-    case (first: Char) ~ (rest: List[Char]) => (first :: rest).mkString.toLong
-  }
-  // ^?：マッチさせる前に条件を設定できる
-  lazy val logn2: Parser[Long] = positiveDigit ~ req(digit) ^? {
-    case first ~ rest if rest.length < 18 => (first :: rest).mkString.toLong
-  }
+//  def parse = (elem('t')~elem('r')~elem('u')~elem('e')) | (elem('f')~elem('a')~elem('l')~elem('s')~elem('e'))
+//  def p2 = ('t'~'r'~'u'~'e') | ('f'~'a'~'l'~'s'~'e')
+//  // ^^^：　マッチした場合の戻り値を指定できる。
+//  def p3: Parser[Boolean] = ('t'~'r'~'u'~'e' ^^^ true) | ('f'~'a'~'l'~'s'~'e' ^^^ false)
+//  def p4: Parser[Boolean] = ('t'~'r'~'u'~'e') | ('f'~'a'~'l'~'s'~'e')
+//  
+//  def positiveDigit = elem('1') | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 
+//  def digit = positiveDigit | '0'
+//  // ^^：　マッチした場合にパターンに含まれる文字の一部を返却できる
+//  def long1: Parser[Long] = positiveDigit ~ rep(digit) ^^ {
+//    case (first: Char) ~ (rest: List[Char]) => (first :: rest).mkString.toLong
+//  }
+//  // ^?：マッチさせる前に条件を設定できる
+//  lazy val logn2: Parser[Long] = positiveDigit ~ req(digit) ^? {
+//    case first ~ rest if rest.length < 18 => (first :: rest).mkString.toLong
+//  }
 }
 
