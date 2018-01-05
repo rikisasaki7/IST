fde5ee49c7dcfc65b903528b587a083057549282
sasaki_riki <sasaki@is-tech.co.jp>
2018-01-05 23:45:54 +0900

    bug-fix test

--- eclipse/ScalaTest/src/chapter7/Displacement.scala
+++ eclipse/ScalaTest/src/chapter7/Displacement.scala
@@ -4,7 +4,8 @@ package chapter7
 object Displacement extends App {
 
   println("---変位 不変-----")
-  
+  println("aaaa")
+
   // 型引数　不変
   class Holder[T](var data: T)
   def add(in: Holder[Int]) { in.data = in.data + 1 }
@@ -12,47 +13,47 @@ object Displacement extends App {
   add(h)
   println(h.data.getClass)
   println(h.data)
-  
+
   val nh = new Holder[Number](33.3d)
   def round(in: Holder[Number]) { in.data = in.data.intValue }
   round(nh)
   println(nh.data.getClass)
   println(nh.data)
-  
+
   val dh = new Holder(33.3d)
 //  round(dh) // コメントアウトを外すとエラーになる。
-  
+
   println()
   println("---変位 共変-----")
-  
+
   class Getable[+T](val data: T)
   def get(in: Getable[Any]) { println("It's " + in.data) }
 
   val gs = new Getable("String")
   get(gs)
-  
+
   def getNum(in: Getable[Number]) = in.data.intValue
   def gd = new Getable(new java.lang.Double(33.3))
   println(getNum(gd))
 
   println()
   println("---変位 反変-----")
-  
+
   class Putable[-T] {
     def put(in: T) { println("Putting " + in) }
   }
   def writeOnly(in: Putable[String]) { in.put("Hellow") }
-  
+
   val p = new Putable[AnyRef]
   writeOnly(p)
 
   println()
   println("---変位 共変と反変-----")
-  
+
   trait DS[-In, +Out] { def apply(i: In): Out }
   val t1 = new DS[Any, Int] { def apply(i: Any) = i.toString.toInt }
   println("DS: " + t1)
-  
+
   def check(in: DS[String, Any]) = in("333")
   print("check: ")
   println(check(t1))
