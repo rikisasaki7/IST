f506451ffb6c05d109655f62d7cf1a0d3a50b809
Sasaki-Riki <unagipretz7@gmail.com>
2018-01-28 21:32:49 +0900

    ・入社前研修のため、Strutsプロジェクトを追加 ・gitignoreを更新

---
+++ eclipse/StrutsTest/src/hello/HelloWorldAction.java
@@ -0,0 +1,21 @@
+package hello;
+
+/**
+ * @author Riki
+ *
+ */
+public class HelloWorldAction {
+	private String name;
+
+	public String execute() throws Exception {
+		return "success";
+	}
+   
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
