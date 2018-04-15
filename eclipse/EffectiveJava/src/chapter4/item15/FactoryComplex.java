dd98c3e07607a9f9dcd7083f220c9975fa1a65fb
sasaki_riki <unagipretz7@gmail.com>
2018-04-15 21:27:55 +0900

    test

--- eclipse/EffectiveJava/src/chapter4/item15/FactoryComplex.java
+++ eclipse/EffectiveJava/src/chapter4/item15/FactoryComplex.java
@@ -5,11 +5,11 @@ package chapter4.item15;
  * 項目１５
  * 可変性を最小限にする
  * classをfinalで定義し、サブクラスができない問題に対する代替案
- * 
+ *
  * ・クラスをfinalにしない
  * ・コンストラクタを全てprivateもしくはパッケージプライベートとする
  * ・publicのコンストラクタの代わりにstaticファクトリメソッドをを追加する
- * 
+ *
  * FactoryComplex：コンストラクタの代わりにstaticファクトリメソッドを持つ不変クラス
  * ※Complexクラスで作って見たのとほぼ同じ
  */
@@ -18,7 +18,7 @@ public class FactoryComplex {
 	private final double re;
 	/* 虚数部 */
 	private final double im;
-	
+
 	/* アクセッサーとミューテータ */
 	public double realPart(){return re;}
 	public double imaginaryPart(){return im;}
@@ -30,14 +30,14 @@ public class FactoryComplex {
 		this.re = re;
 		this.im = im;
 	}
-	
+
 	/*
 	 * staticファクトリメソッド
 	 */
 	public static FactoryComplex valueOf(double re, double im){
 		return new FactoryComplex(re, im);
 	}
-	
+
 	/*
 	 * 異なる用途でも使えるstaticファクトリメソッド。
 	 * この場合は単なる複素数を生成するのではなく、座標軸に基づく複素数生成を行う。
@@ -46,5 +46,4 @@ public class FactoryComplex {
 	public static FactoryComplex valueOfPolar(double r, double theta){
 		return new FactoryComplex(r * Math.cos(theta), r* Math.sin(theta));
 	}
-
 }
